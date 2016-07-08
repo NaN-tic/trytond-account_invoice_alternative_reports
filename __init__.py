8059afcb88a5d667a245f4bcd84e134e230932f2
Bernat Brunet Torruella <bernat@nan-tic.com>
2016-07-08 13:24:10 +0200

    Resolve some problems with the multi reports when they are create manualy not throw code. Now this report only use jasper reports. Add config for the main report. Add account_invoice_jreport_cache depenency for the correct work and use the cache.

--- __init__.py
+++ __init__.py
@@ -2,13 +2,19 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from .invoice import *
+from .configuration import *
 
 
 def register():
     Pool.register(
+        AccountConfiguration,
+        AccountConfigurationCompany,
         Invoice,
         PartyAlternativeReport,
         module='account_invoice_alternative_reports', type_='model')
     Pool.register(
         PrintInvoice,
         module='account_invoice_alternative_reports', type_='wizard')
+    Pool.register(
+        InvoiceReport,
+        module='account_invoice_alternative_reports', type_='report')
